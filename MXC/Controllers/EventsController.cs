64fd51b428c23673ede7cef2821e5b7481508154
szabg <szabg@DESKTOP-P71HNV1>
2026-07-04 18:36:13 +0200

    EventsController crud endpoint

--- MXC/Controllers/EventsController.cs
+++ MXC/Controllers/EventsController.cs
@@ -35,5 +35,62 @@ namespace MXC.Controllers
 
             return Ok(_testEvents);
         }
+
+        [HttpGet]
+        [Route("GetById")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            await Task.Delay(1000); // Simulate some async work
+
+            var record = _testEvents.FirstOrDefault(e => e.Id == id);
+
+            if (record == null)
+                return NotFound();
+
+            return Ok(record);
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> Create([FromBody] Event newEvent)
+        {
+            await Task.Delay(1000); // Simulate some async work
+
+            newEvent.Id = Guid.NewGuid();
+            _testEvents.Add(newEvent);
+            return CreatedAtAction(nameof(GetById), new { id = newEvent.Id }, newEvent);
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public async Task<IActionResult> Update([FromBody] Event updatedEvent)
+        {
+            await Task.Delay(1000); // Simulate some async work
+            var existingEvent = _testEvents.FirstOrDefault(e => e.Id == updatedEvent.Id);
+
+            if (existingEvent == null)
+                return NotFound();
+
+            existingEvent.Name = updatedEvent.Name;
+            existingEvent.Location = updatedEvent.Location;
+            existingEvent.Country = updatedEvent.Country;
+            existingEvent.Capacity = updatedEvent.Capacity;
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await Task.Delay(1000); // Simulate some async work
+
+            var existingEvent = _testEvents.FirstOrDefault(e => e.Id == id);
+            if (existingEvent == null)
+                return NotFound();
+            _testEvents.Remove(existingEvent);
+
+            return Ok();
+        }
     }
 }
